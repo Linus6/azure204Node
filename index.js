0952003ca2a3176264760b95cf06391b4991fd04
Linus6 <sunilmathad6@gmail.com>
2023-09-20 22:11:03 +0530

    console change

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(8080, () => {
-    console.log('The application is listening on port 3000!');
+    console.log('The application is listening on port 8080!');
 })
